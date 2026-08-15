2ceef6326ad1b4d3aff5b4726629a290f3812f5c
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-15 05:48:12 +0100

    Update .bash

--- .bash
+++ .bash
@@ -4,3 +4,5 @@ dotnet add package Microsoft.CodeAnalysis.CSharp
 git add .github/workflows/build-and-test.yml
 git commit -m "Add CI build and test workflow"
 git push origin main
+dotnet add package Microsoft.CodeAnalysis.CSharp
+dotnet add package Microsoft.CodeAnalysis.Analyzers
